14090cfb1ffd2ab04b8f0b4c326e72bdb8bac3a6
xhbsh <qq260342855@gmail.com>
2024-05-01 22:58:15 +0800

    注释部分代码

--- src/main/java/top/sducraft/mixins/lbd/LeadItemMixin.java
+++ src/main/java/top/sducraft/mixins/lbd/LeadItemMixin.java
@@ -1,15 +1,12 @@
 package top.sducraft.mixins.lbd;
 
-import com.mojang.datafixers.kinds.IdF;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.LeadItem;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,7 +15,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import top.sducraft.Settings;
 
-import java.awt.*;
 import java.util.Iterator;
 import java.util.List;
 
@@ -30,17 +26,17 @@ public abstract class LeadItemMixin extends Item {
         super(properties);
     }
 
-    @Inject(method = "bindPlayerMobs",at = @At(value = "HEAD"))
+    @Inject(method = "bindPlayerMobs",at = @At(value = "HEAD"), cancellable = true)
     private static void leadBandBeeDisable(Player player, Level level, BlockPos blockPos, CallbackInfoReturnable<InteractionResult> cir){
-       if(Settings.leadBandBeeDisable) {
-           int i = blockPos.getX();
-           int j = blockPos.getY();
-           int k = blockPos.getZ();
-           List<Mob> A = level.getEntitiesOfClass(Mob.class, new AABB((double)i - 7.0, (double)j - 7.0, (double)k - 7.0, (double)i + 7.0, (double)j + 7.0, (double)k + 7.0));
-            Iterator B = A.iterator();
-             if (A == EntityType.BEE){
-            cir.setReturnValue(InteractionResult.PASS);
-        }
-        }
+//       if(Settings.leadBandBeeDisable) {
+//           int i = blockPos.getX();
+//           int j = blockPos.getY();
+//           int k = blockPos.getZ();
+//           List<Mob> A = level.getEntitiesOfClass(Mob.class, new AABB((double)i - 7.0, (double)j - 7.0, (double)k - 7.0, (double)i + 7.0, (double)j + 7.0, (double)k + 7.0));
+//            Iterator<Mob> B = A.iterator();
+//             if ( B == EntityType.BEE){
+//            cir.setReturnValue(InteractionResult.FAIL);
+//        }
+//        }
     }
 }
