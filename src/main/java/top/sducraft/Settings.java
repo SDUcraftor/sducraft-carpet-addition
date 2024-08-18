c5034798311b5124a49f188f294b97fe5b4a65f0
xhbsh <260342855@qq.com>
2024-08-18 22:40:53 +0800

    移除规则 禁用盔甲架收到潜影贝导弹伤害 禁用栓绳栓蜜蜂 添加规则 树苗放置忽略条件 禁用音符盒方块交互

--- src/main/java/top/sducraft/Settings.java
+++ src/main/java/top/sducraft/Settings.java
@@ -4,22 +4,25 @@ import carpet.api.settings.Rule;
 
 public class Settings {
     public static final String sdu= "SDU";
+    public static final String sdu3rd= "SDU_3RD";
+    public static final String blocksdu= "BLOCK_SDU";
 
-    @Rule(
-            categories = {sdu}
-    )
+    @Rule(categories = {sdu,sdu3rd})
     public static boolean skipCropLightCheck = false;
 
-    @Rule(
-            categories = {sdu}
-    )
+    @Rule(categories = {sdu,sdu3rd})
     public static boolean brittleDeepSlate = false;
-    @Rule(
-            categories = {sdu}
-    )
+
+    @Rule(categories = {sdu,blocksdu})
     public static boolean notebookIgnoreupdate = false;
-    @Rule(
-            categories = {sdu}
-    )
+
+    @Rule(categories = {sdu,blocksdu})
     public static boolean noFallingBlock = false;
+
+    @Rule(categories = {sdu,blocksdu})
+    public static boolean saplingPlaceIgnoreCondition = false;
+
+    @Rule(categories = {sdu,blocksdu})
+    public static boolean disablenoteboxinteraction = false;
+
 }
